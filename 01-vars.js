8ccd2dc2ad20d46ee72e0eb782dd70b794dbc0f4
lliney <lliney1989@gmail.com>
2022-03-20 17:23:39 +0200

    Add files via upload

---
+++ 01-vars.js
@@ -0,0 +1,26 @@
+/*const age = 10;
+console.log('Age: ', age);
+const type = typeof age; // typeof вертає тип змінної
+console.log(type);
+const shuldRenew = confirm('Do you want renew?'); // confirm очікує від кристувача так/ні
+console.log(shuldRenew);
+let quanity = prompt('input quanity'); //promt запитує стрічку у користувача
+quanity = Number(quanity);
+console.log(quanity);
+console.log(typeof (quanity)); */
+//let elementWith = '50px';
+//const result = Number.parseInt(elementWith); //parseInt - пробує отримати число
+//console.log(result);
+//let elementHight = '50.75px';
+//elementHight = Number.parseFloat(elementHight);
+//console.log(elementHight);
+//let sellery = 1380.57867;
+//sellery = sellery.toFixed(2);
+//sellery = Number(sellery);
+//console.log(sellery);
+//console.log(Math.pow(2, 5)); //невелика бібліотека для математики
+//console.log(2 ** 5);
+const max = 80;
+const min = 10;
+const result = Math.round(Math.random ()* (max - min) + min);
+console.log(result);
